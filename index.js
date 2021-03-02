603a3b624260af9d41f479e46a6c8c4d3e066135
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-02 11:24:21 -0700

    added some more base styles to theme game

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let randomWordsArr = ["javascript", "nodemon", "interpolation"];
 
 let wins = 0;
 let losses = 0;
-let guessesLeft = 9;
+let guessesLeft = 10;
 let lettersGuessed = [];
 let computerGuess = [];
 // Got random word to appear now need to loop over word to render correct amount of dashes fro hangman
@@ -12,3 +12,5 @@ window.onload = function () {
   computerGuess.push(compRandomWord);
   console.log(computerGuess[0]);
 };
+
+//if letter has already been guessed stop user from using letter again
