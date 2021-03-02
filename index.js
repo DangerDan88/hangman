b33856c0529e53d985752d3b7e4bb65520192bc8
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-01 19:00:23 -0700

    got random word to console log need to loop to get right amount of dashes to appear

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const randomWordsArr = ["javascript", "nodemon", "interpolation"];
+let randomWordsArr = ["javascript", "nodemon", "interpolation"];
 
-console.log(randomWordsArr);
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let lettersGuessed = [];
+let computerGuess = [];
+// Got random word to appear now need to loop over word to render correct amount of dashes fro hangman
+window.onload = function () {
+  let compRandomWord =
+    randomWordsArr[Math.floor(Math.random() * randomWordsArr.length)];
+  computerGuess.push(compRandomWord);
+  console.log(computerGuess[0]);
+};
