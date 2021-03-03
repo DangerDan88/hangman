44fc7e6ef9922e7883bc9729be697a8a6b868c32
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-03 16:26:25 -0700

    trying to loop computrt random word thru the letters

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onload = function () {
   let compRandomWord =
     randomWordsArr[Math.floor(Math.random() * randomWordsArr.length)];
   computerGuess.push(compRandomWord);
-  console.log(computerGuess[0]);
+  // console.log(computerGuess);
 };
 
 //if letter has already been guessed stop user from using letter again
@@ -17,5 +17,12 @@ document.onkeyup = function (event) {
   let userGuess = event.key;
   lettersGuessed.push(userGuess);
   console.log(userGuess);
-  //console.log(computerGuess[0]);
+
+  function loopArr() {
+    console.log(computerGuess);
+    // let letterStr = computerGuess.charAt(0);
+    // console.log(letterStr);
+    //Loop over computer random guess letters and if statment it over the user guess dont have to loop full array jus the random word
+  }
+  loopArr();
 };
