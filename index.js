2eda819feccb509521151f31659cf64d8d46b631
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-05 11:40:26 -0700

    getting letters to work when comparing to computer guess

--- index.js
+++ index.js
@@ -4,35 +4,23 @@ let losses = 0;
 let guessesLeft = 10;
 let lettersGuessed = [];
 let computerGuess = "";
+let correctGuess = [];
 // Got random word to appear now need to loop over word to render correct amount of dashes fro hangman
 window.onload = function () {
   let compRandomWord =
     randomWordsArr[Math.floor(Math.random() * randomWordsArr.length)];
   computerGuess = compRandomWord;
-  //console.log(computerGuess);
+  console.log(computerGuess);
 };
 
 //if letter has already been guessed stop user from using letter again
 document.onkeyup = function (event) {
   let userGuess = event.key;
   lettersGuessed.push(userGuess);
-  //console.log(userGuess);
+  guessesLeft--;
+  console.log(guessesLeft);
+  console.log(computerGuess.includes(userGuess));
 
-  function loopArr() {
-    for (let i = 0; i < computerGuess.length; i++) {
-      // console.log(computerGuess[i]);
-      let letterStr = computerGuess.charAt(i).split("");
-      console.log(letterStr);
-      if (userGuess === letterStr) {
-        console.log("its a match");
-      } else if (userGuess !== letterStr) {
-        console.log("not a match");
-        guessesLeft -= 1;
-        console.log("guess", guessesLeft);
-      }
-    }
-    // console.log(computerGuess);
-    //need to add logic to update score letters guessed and hangman on right and wrong guess fix else statement logging not a match probably a prolblem with for loop
-  }
-  loopArr();
+  console.log(userGuess);
 };
+//TOdO figure out how to check user guess is a match to any letter in the string
