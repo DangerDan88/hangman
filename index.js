fb1fa891971c4ff7112e8c037f5abae6ccaac69a
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-09 13:20:17 -0700

    we working logic ran into some issues with previous method

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 let randomWordsArr = ["javascript", "nodemon", "interpolation"];
 let guessesLeft = 10;
 let lettersGuessed = [];
-let computerGuess = [];
+let computerGuess = "nodemon";
 let correctGuess = [];
+//let userGuess = "";
 // Got random word to appear now need to loop over word to render correct amount of dashes fro hangman
-window.onload = function () {
-  let compRandomWord =
-    randomWordsArr[Math.floor(Math.random() * randomWordsArr.length)];
-  computerGuess.push(compRandomWord);
-  console.log(computerGuess);
-};
+//window.onload = function () {
+//  let compRandomWord =
+//    randomWordsArr[Math.floor(Math.random() * randomWordsArr.length)];
+//  computerGuess = compRandomWord;
+//  console.log(computerGuess);
+//};
 
 //if letter has already been guessed stop user from using letter again
-document.onkeyup = function (event) {
-  let userGuess = event.key;
-  lettersGuessed.push(userGuess);
-  console.log(guessesLeft);
-  if (computerGuess.includes(userGuess) && guessesLeft > 0) {
-    console.log("thats a good guess");
-    correctGuess.push(userGuess);
-    lettersGuessed.push(userGuess);
-    //console.log(correctGuess.join(""));
-    console.log(computerGuess);
-    console.log(userGuess);
-    document.querySelector(".gameContainer").innerHTML = correctGuess;
-  } else if (correctGuess == computerGuess) {
-    alert("Thats a win");
-  } else if (guessesLeft < 1) {
-    alert("Game is over try again ");
-  } else {
-    guessesLeft--;
-    lettersGuessed.push(userGuess);
+document.onkeyup = function logUserKey(event) {
+  let userKey = event.key;
+  lettersGuessed.push(userKey);
+  console.log(lettersGuessed);
+  console.log(computerGuess);
+  if (userKey == computerGuess) {
+    console.log("it a match");
   }
 
   document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
 };
-
-//TODO figure out to have a user win my comparison isnt working and need to figure out dashes for the hangman
