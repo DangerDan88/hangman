0112cfba869950b6cf4a9b6138c7c01c47f43402
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-03-10 13:02:58 -0700

    game functio ing correct need to clean up some stuff and figure out hangman dashes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let guessesLeft = 10;
 let lettersGuessed = [];
 let computerGuess = "nodemon";
 let correctGuess = [];
+let wrongGuess = [];
+
 //let userGuess = "";
 // Got random word to appear now need to loop over word to render correct amount of dashes fro hangman
 //window.onload = function () {
@@ -19,15 +21,18 @@ document.onkeyup = function logUserKey(event) {
   console.log(lettersGuessed.join(""));
   console.log(computerGuess);
   guessesLeft--;
-  if (lettersGuessed.join("") === computerGuess) {
+  if (correctGuess.join("") === computerGuess) {
     console.log("congrats you win");
   } else if (guessesLeft < 1) {
     console.log("GameOver");
   } else if (computerGuess.includes(userKey) && guessesLeft > 0) {
     console.log("thats a good guess");
+    correctGuess.push(userKey);
+    document.querySelector(".correctGuess").innerHTML = correctGuess;
   } else {
     console.log("nice try guess again");
-  }
+    wrongGuess.push(userKey);
 
-  document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
+    document.querySelector("#lettersGuessed").innerHTML = wrongGuess;
+  }
 };
